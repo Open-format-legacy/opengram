8a0bb54383dbc3f5dd35b1f7fdb3e6eb713f6b72
Benjamin Reid <ben@simpleweb.co.uk>
2022-09-29 15:40:07 +0100

    Install open format

---
+++ NOTES.md
@@ -0,0 +1,15 @@
+## Steps
+
+Add the Open Format React SDK and ethers.
+
+```shell
+yarn add @simpleweb/open-format-react ethers
+```
+
+Wrap \_app.tsx in the <OpenFormatProvider>
+
+Get a UUID token from https://www.uuidgenerator.net/
+
+Set the config with the token c94972f0-b156-4cc8-b390-22d2b04cd0d7 (or new one) and set network to mumbai
+
+Sometimes next can crap out, delete the .next cache
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
+import { OpenFormatProvider } from "@simpleweb/open-format-react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <OpenFormatProvider
+        config={{
+          network: "mumbai",
+          factory: "c94972f0-b156-4cc8-b390-22d2b04cd0d7",
+        }}
+      >
+        <Component {...pageProps} />
+      </OpenFormatProvider>
     </>
   );
 }
