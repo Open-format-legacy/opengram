4939aeeafddd1471e5558f691b499b6f0234c5aa
Benjamin Reid <ben@simpleweb.co.uk>
2022-10-03 11:03:13 +0100

    Add mint heart

--- components/post.tsx
+++ components/post.tsx
@@ -1,16 +1,17 @@
-import Avatar from "./avatar";
 import Link from "next/link";
 import { useState } from "react";
+import Avatar from "./avatar";
+import MintHeart from "./mint-heart";
 
 type Attribute = "camera" | "aperture" | "focalLength" | "iso";
 
 interface Props {
+  id: string;
   name: string;
   description: string;
   imageUrl: string;
   creator: string;
   createdAt: string;
-  liked: boolean;
   minimal?: boolean;
   metadata?: {
     [key in Attribute]?: string;
@@ -18,12 +19,12 @@ interface Props {
 }
 
 function Post({
+  id,
   name,
   description,
   imageUrl,
   creator,
   createdAt,
-  liked,
   minimal,
   metadata,
 }: Props) {
@@ -76,9 +77,7 @@ function Post({
           <div className="mb-2">
             <div className="-ml-[2px]">
               {/* @TODO hook up to minting */}
-              <button className="block">
-                <Heart liked={liked} />
-              </button>
+              <MintHeart id={id} />
             </div>
           </div>
           <h6 className="mb-[2px]">
@@ -136,40 +135,6 @@ function Post({
         </div>
       </div>
     </>
-  );
-}
-
-function Heart({ liked }: { liked: boolean }) {
-  if (liked) {
-    return (
-      <div className="text-red-500">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-        </svg>
-      </div>
-    );
-  }
-
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-      />
-    </svg>
   );
 }
 
